45dfc45b71bba1d887dcf36ce77e4e45202efd47
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-06-24 22:25:55 +0200

    Include order button on the footer

--- src/index.js
+++ src/index.js
@@ -100,7 +100,18 @@ function Footer() {
     console.log(isOpen)
     // if(hour >= openHour && hour <= closeHour) alert("We're currently open!")
     // else alert("Sorry we are ")
-    return <footer className="footer">{new Date().toLocaleTimeString()}. We're currently open</footer>
+    return (
+      <footer className="footer">
+        {isOpen && (
+          <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.
+          </p>
+          <button className="btn">Order</button>
+          </div>
+        )}
+      </footer>
+    )
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
