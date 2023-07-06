e0ee397f417ebd5494417b6a42a80b987d9ff793
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-07-06 11:10:21 +0200

    Move the Footer component to its own file

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Order from "./Order";
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00, and {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+}
+
+export default Footer;
