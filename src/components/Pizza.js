e0615c84df7e91abbbef8a114649d3d28acc0952
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-07-07 11:08:46 +0200

    Create pizza component

---
+++ src/components/Pizza.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+
+export default Pizza;
