1acbaeaedbc89dad25c5c024bcb486ecad1f4c43
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-07-09 11:05:32 +0200

    Move the Header component to its file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header footer">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+export default Header;
