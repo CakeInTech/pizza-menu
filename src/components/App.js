da8b16d0aef7da67fa834a8ccbcd749b8f62d0b8
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-07-10 11:04:20 +0200

    Create App file for the app component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Menu from "./Menu";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
